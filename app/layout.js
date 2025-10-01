aadf403ffff7df15d62a2678cf0d9af7555e0b86
Ansh Gandhi <anshgandhi@hotmail.com>
2025-10-01 15:55:28 -0700

    setup header, convex DB, inngest cron jobs, clerk authentication

---
+++ app/layout.js
@@ -0,0 +1,33 @@
+import { Inter } from "next/font/google";
+import "./globals.css";
+import Header from "@/components/Header";
+import { ConvexClientProvider } from "@/components/convex-client-provider";
+import { ClerkProvider } from "@clerk/nextjs";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Expense Management",
+  description: "Manage expenses between friends and groups",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <link rel="icon" href="/logos/expense_management_logo.png" sizes="any" />
+      </head>
+      <body
+        className={`${inter.className}`}
+      >
+        <ClerkProvider>
+          <ConvexClientProvider>
+            <Header />
+
+            <main className="min-h-screen">{children}</main>
+          </ConvexClientProvider>
+        </ClerkProvider>
+      </body>
+    </html>
+  );
+}
